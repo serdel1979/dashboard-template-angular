a9c70237692b2476e1340861fbf68231d91ce0a7
Sergio De Luca <sdlbsso@gmail.com>
2023-01-27 20:02:49 -0300

    Template con login y dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NotFountComponent } from './pages/not-fount/not-fount.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { AuthRoutingModule } from './auth/auth.routing';
+import { NotFountComponent } from './not-fount/not-fount.component';
+import { PagesRoutingModule } from './pages/pages.routing';
 
 const routes: Routes = [
+
   {
-      path: '', 
-      component: PagesComponent,
-      children: [
-        {path: 'dashboard', component: DashboardComponent},
-        {path: 'progress', component: ProgressComponent},
-        {path: 'grafica1', component: Grafica1Component},
-        {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-      ]
+    path: 'auth',
+    loadChildren: ()=> import('./auth/auth.module').then(m => m.AuthModule)
   },
-
-
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-
-
+  {
+    path: 'dashboard',
+    loadChildren: ()=> import('./pages/pages.module').then(m => m.PagesModule),
+  },
   {path: '**', component: NotFountComponent},
 ]
 
@@ -32,7 +21,9 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports:[
     RouterModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NotFountComponent } from './pages/not-fount/not-fount.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PagesComponent } from './pages/pages.component';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+import { NotFountComponent } from './not-fount/not-fount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NotFountComponent,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+    NotFountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
