b1a13fff6e24d04532499bfcd0a9a00a76485e18
Sergio De Luca <sdlbsso@gmail.com>
2023-01-27 18:08:10 -0300

    Dashboard paginas y login con sistema de rutas

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styles: [
-  ]
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
 
